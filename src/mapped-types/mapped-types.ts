086effe14902d976a6acff7e9035d609927664ad
Alan N <alan.nguy@outlook.com>
2021-09-24 18:07:45 -0400

    mapped type section work

--- src/mapped-types/mapped-types.ts
+++ src/mapped-types/mapped-types.ts
@@ -5,4 +5,29 @@ type Fruit = {
 }
 
 type ArbitraryRecord = { [k: string]: Fruit } // object with any prop key can fit a Fruit type
+
 type MyRecord = { [Fruitkey in 'apple' | 'cherry']: Fruit } // only apple and cherry prop exist in this type.
+
+// better way to make our types more generalized with some type params
+// Here we build a type with a set of known keys with a consistent value type.
+type BetterRecord<KeyType extends string, ValueType> = {
+  [Key in KeyType]: ValueType
+}
+
+// If you want to create a partial type from another type,
+
+// Our full type here
+type LegitType = {
+  id: number
+  name: string
+  description: string
+  isCompleted: boolean
+}
+
+// As we know keyof operator produces a string or numeric literal union of its keys.
+type PartialBuilder<ValueType, Keys extends keyof ValueType> = {
+  [key in Keys]: ValueType[key]
+}
+
+// The partial is here
+type PartOfLegitType = PartialBuilder<LegitType, 'id' | 'name'>
